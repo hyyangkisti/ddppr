39ab7dba2332ccc7661f929f4abf13bd5489c424
Young Jin Kim <kimyoungjin06@gmail.com>
2020-11-18 02:51:33 +0000

    Add items data and its tables.

--- js/main.js
+++ js/main.js
@@ -16,6 +16,99 @@ $(document).ready(function () {
         }
     )
 
+    var table = $('#DiseaseTB').DataTable({
+        ajax: {
+            'url':'disease.json', 
+            'dataSrc':''
+        },
+        responsive: true,
+        orderMulti: true,
+        order : [[1, 'desc']],
+        columns: [
+            {"data": "disease"}
+        ],
+        "language": {
+            "emptyTable": "데이터가 없어요.",
+            "lengthMenu": "페이지당 _MENU_ 개씩 보기",
+            "info": "현재 _START_ - _END_ / _TOTAL_건",
+            "infoEmpty": "데이터 없음",
+            "infoFiltered": "( _MAX_건의 데이터에서 필터링됨 )",
+            "search": "에서 검색: ",
+            "zeroRecords": "일치하는 데이터가 없어요.",
+            "loadingRecords": "로딩중...",
+            "processing":     "잠시만 기다려 주세요...",
+            "paginate": {
+                "next": "다음",
+                "previous": "이전"
+            }
+        },
+        dom : 'Blfrtip',
+//         buttons:[{
+// 		}]
+    });
+    var table = $('#DrugTB').DataTable({
+        ajax: {
+            'url':'drug.json', 
+            'dataSrc':''
+        },
+        responsive: true,
+        orderMulti: true,
+        order : [[1, 'desc']],
+        columns: [
+            {"data": "drug"}
+        ],
+        "language": {
+            "emptyTable": "데이터가 없어요.",
+            "lengthMenu": "페이지당 _MENU_ 개씩 보기",
+            "info": "현재 _START_ - _END_ / _TOTAL_건",
+            "infoEmpty": "데이터 없음",
+            "infoFiltered": "( _MAX_건의 데이터에서 필터링됨 )",
+            "search": "에서 검색: ",
+            "zeroRecords": "일치하는 데이터가 없어요.",
+            "loadingRecords": "로딩중...",
+            "processing":     "잠시만 기다려 주세요...",
+            "paginate": {
+                "next": "다음",
+                "previous": "이전"
+            }
+        },
+        dom : 'Blfrtip',
+//         buttons:[{
+// 		}]
+    });
+    var table = $('#ProteinTB').DataTable({
+        ajax: {
+            'url':'protein.json', 
+            'dataSrc':''
+        },
+        responsive: true,
+        orderMulti: true,
+        order : [[1, 'desc']],
+        columns: [
+            {"data": "protein"}
+        ],
+        "language": {
+            "emptyTable": "데이터가 없어요.",
+            "lengthMenu": "페이지당 _MENU_ 개씩 보기",
+            "info": "현재 _START_ - _END_ / _TOTAL_건",
+            "infoEmpty": "데이터 없음",
+            "infoFiltered": "( _MAX_건의 데이터에서 필터링됨 )",
+            "search": "에서 검색: ",
+            "zeroRecords": "일치하는 데이터가 없어요.",
+            "loadingRecords": "로딩중...",
+            "processing":     "잠시만 기다려 주세요...",
+            "paginate": {
+                "next": "다음",
+                "previous": "이전"
+            }
+        },
+        dom : 'Blfrtip',
+//         buttons:[{
+// 		}]
+    });
+
+    
+    
     var table = $('#DiToDr').DataTable({
         ajax: {
             'url':'DIS_DRG_test.json', 
