5fad28ba7ac38c991e29070b53db4b3b91b4ff6d
Young Jin Kim <kimyoungjin06@gmail.com>
2020-11-24 09:27:13 +0000

    Test

---
+++ js/main2.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    
+    // Get json data    
+    function loadSelectItems(select, items) {
+        var options = '';
+        $.each(items, function(key, value) {
+            options += '<option value=' + value.disease + '>' + value.disease + '</option>';
+        });
+        select.empty();
+        select.append(options);
+        select.selectpicker('refresh');
+    }
+
+    function get_drugs(select, items) {
+        var options = '';
+        $.each(items, function(key, value) {
+            options += '<option value=' + value.drug + '>' + value.drug +', '+ value.PA +' ' + value.corr + '</option>';
+        });
+        select.empty();
+        select.append(options);
+        select.selectpicker('refresh');
+    }
+    const url = './disease.json';
+    $.getJSON(url, function (items) {loadSelectItems($('#Diseases-sp'), items)});
+
+    // Action for select disease
+    $(function() {
+        $('#Diseases-sp').on("changed.bs.select", function(e, clickedIndex) {
+            const drug_json = './json/drug/drugs_"' + this.value + '".json';
+            $.getJSON(drug_json, function (items) {get_drugs($('#Drugs-sp'), items)});  
+        });
+    });
+
+});
