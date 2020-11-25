295f2955eb4797a3a9630cb56eba585442e6af89
Young Jin Kim <kimyoungjin06@gmail.com>
2020-11-25 08:44:01 +0000

    Delete old files.

--- js/main2.js
+++ js/main2.js
@@ -3,25 +3,29 @@ $(document).ready(function () {
     function loadSelectItems(select, items) {
         var options = '';
         $.each(items, function(key, value) {
-            options += '<option value=' + value.disease + '>' + value.disease + '</option>';
+            options += '<option value=' + value.disease + 'data-tokens=' + value.disease_tk + '>' + value.disease + '</option>';
         });
         select.empty();
         select.append(options);
         select.selectpicker('refresh');
     }
 
+    const url = './disease.json';
+    $.getJSON(url, function (items) {loadSelectItems($('#Diseases-sp'), items)});
+    
     function get_drugs(select, items) {
         var options = '';
         $.each(items, function(key, value) {
-            options += '<option value=' + value.drug + '>' + value.drug +', '+ value.PA +' ' + value.corr + '</option>';
+            options += '<option value=' + value.drug + 'data-tokens=' + value. + 'data-subtext=' + value.No + '>' + value.drug +', '+ value.PA +' ' + value.corr + '</option>';
+//             <option data-tokens="ketchup mustard">Hot Dog, Fries and a Soda</option>
+//             data-tokens: 내부에서 값을 읽어오는 방식(띄어쓰기 > 언더바로 변경해야함)
+//             data-subtext="Heinz" : subtext for PA
         });
         select.empty();
         select.append(options);
         select.selectpicker('refresh');
     }
-    const url = './disease.json';
-    $.getJSON(url, function (items) {loadSelectItems($('#Diseases-sp'), items)});
-
+    
     // Action for select disease
     $(function() {
         $('#Diseases-sp').on("changed.bs.select", function(e, clickedIndex) {
