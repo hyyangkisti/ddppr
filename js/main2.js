d4f061ab332e97a398d224bf4a935c1870ccc56c
Young Jin Kim <kimyoungjin06@gmail.com>
2020-11-25 09:26:22 +0000

    Test

--- js/main2.js
+++ js/main2.js
@@ -34,4 +34,113 @@ $(document).ready(function () {
         });
     });
 
+    
+    // ************* ABOUT DATATABLES ***************
+    $.fn.dataTable.ext.search.push(
+        function(settings, data, dataIndex){
+            var min = Date.parse($('#fromDate').val());
+            var max = Date.parse($('#toDate').val());
+            var targetDate = Date.parse(data[5]);
+
+            if( (isNaN(min) && isNaN(max) ) || 
+                (isNaN(min) && targetDate <= max )|| 
+                ( min <= targetDate && isNaN(max) ) ||
+                ( targetDate >= min && targetDate <= max) ){ 
+                    return true;
+            }
+            return false;
+        }
+    )
+    
+    // ************* DRUG ***************
+    var table = $('#DrugTB').DataTable({
+        ajax: {
+            'url':'json/drug/drugs_COVID_19.json', 
+            'dataSrc':''
+        },
+        responsive: true,
+        orderMulti: true,
+        order : [[1, 'desc']],
+        columns: [
+            {"data": "No"},
+            {"data": "item"}
+            {"data": "PA"}
+            {"data": "corr"}
+        ],
+        "language": {
+            "emptyTable": "데이터가 없어요.",
+            "lengthMenu": "페이지당 _MENU_ 개씩 보기",
+            "info": "현재 _START_ - _END_ / _TOTAL_건",
+            "infoEmpty": "데이터 없음",
+            "infoFiltered": "( _MAX_건의 데이터에서 필터링됨 )",
+            "search": "에서 검색: ",
+            "zeroRecords": "일치하는 데이터가 없어요.",
+            "loadingRecords": "로딩중...",
+            "processing":     "잠시만 기다려 주세요...",
+            "paginate": {
+                "next": "다음",
+                "previous": "이전"
+            }
+        },
+        dom : 'Blfrtip',
+        buttons:[{
+		}]
+    });
+    
+    
+    
+        /* Column별 검색기능 추가 */
+    $('#DrugTB_filter').prepend('<select id="select"></select>');
+    $('#DrugTB > thead > tr').children().each(function (indexInArray, valueOfElement) { 
+        $('#select').append('<option>'+valueOfElement.innerHTML+'</option>');
+    });
+    
+    $('.dataTables_filter input').unbind().bind('keyup', function () {
+        var colIndex = document.querySelector('#select').selectedIndex;
+        table.column(colIndex).search(this.value).draw();
+    });
+    
+    // ************* PROTEIN ***************
+    var table = $('#ProteinTB').DataTable({
+        ajax: {
+            'url':'json/protein/drugs_COVID_19.json', 
+            'dataSrc':''
+        },
+        responsive: true,
+        orderMulti: true,
+        order : [[1, 'desc']],
+        columns: [
+            {"data": "No"},
+            {"data": "item"}
+            {"data": "corr"}
+        ],
+        "language": {
+            "emptyTable": "데이터가 없어요.",
+            "lengthMenu": "페이지당 _MENU_ 개씩 보기",
+            "info": "현재 _START_ - _END_ / _TOTAL_건",
+            "infoEmpty": "데이터 없음",
+            "infoFiltered": "( _MAX_건의 데이터에서 필터링됨 )",
+            "search": "에서 검색: ",
+            "zeroRecords": "일치하는 데이터가 없어요.",
+            "loadingRecords": "로딩중...",
+            "processing":     "잠시만 기다려 주세요...",
+            "paginate": {
+                "next": "다음",
+                "previous": "이전"
+            }
+        },
+        dom : 'Blfrtip',
+        buttons:[{
+		}]
+    });
+        /* Column별 검색기능 추가 */
+    $('#ProteinTB_filter').prepend('<select id="select"></select>');
+    $('#ProteinTB > thead > tr').children().each(function (indexInArray, valueOfElement) { 
+        $('#select').append('<option>'+valueOfElement.innerHTML+'</option>');
+    });
+    
+    $('.dataTables_filter input').unbind().bind('keyup', function () {
+        var colIndex = document.querySelector('#select').selectedIndex;
+        table.column(colIndex).search(this.value).draw();
+    });
 });
