14be15f9fa9c6fd3b6385500772b18d89a9e398f
Young Jin Kim <kimyoungjin06@gmail.com>
2020-11-26 02:49:37 +0000

    Test

--- js/main2.js
+++ js/main2.js
@@ -68,8 +68,42 @@ $(document).ready(function () {
                 buttons:[{
                 }]
             });
-            $('#example').DataTable().ajax.reload();
+            $('#DrugTB').DataTable().ajax.reload();
             
+            $('#ProteinTB').DataTable().destroy();
+            var table = $('#ProteinTB').DataTable({
+                ajax: {
+                    'url':'json/protein/drugs_COVID_19.json', 
+                    'dataSrc':''
+                },
+                responsive: true,
+                orderMulti: true,
+                order : [[1, 'desc']],
+                columns: [
+                    {"data": "No"},
+                    {"data": "item"},
+                    {"data": "corr"}
+                ],
+                "language": {
+                    "emptyTable": "데이터가 없어요.",
+                    "lengthMenu": "페이지당 _MENU_ 개씩 보기",
+                    "info": "현재 _START_ - _END_ / _TOTAL_건",
+                    "infoEmpty": "데이터 없음",
+                    "infoFiltered": "( _MAX_건의 데이터에서 필터링됨 )",
+                    "search": "에서 검색: ",
+                    "zeroRecords": "일치하는 데이터가 없어요.",
+                    "loadingRecords": "로딩중...",
+                    "processing":     "잠시만 기다려 주세요...",
+                    "paginate": {
+                        "next": "다음",
+                        "previous": "이전"
+                    }
+                },
+                dom : 'Blfrtip',
+                buttons:[{
+                }]
+            });
+            $('#DrugTB').DataTable().ajax.reload();
             
             
 //             $.getJSON(drug_json, function (items) {get_drugs($('#Drugs-sp'), items)});  
